20c3844c8b9a83de63156c9c06dabfc27c5e07e0
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2022-09-11 21:47:22 -0500

    finished most of assignment delete still nonfunctional

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const app = express();
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: false}))
 const productRouter = require('./routes/Products')
+app.set('views', __dirname + '/templates')
+app.set('view engine', 'twig')
+
+app.get('/', (req, res) => {
+    res.render("home", {name: 'World!', users: [
+        {name: 'Brian Barker', email: 'bmbarker@fullsail.com'},
+        {name: 'Rick Grimes', email: 'rgrimes@fullsail.com'},
+        {name: 'Bobby Joe', email: 'bobbyj@fullsail.com'}
+    ]})
+})
 
 app.use("/products", productRouter)
 
